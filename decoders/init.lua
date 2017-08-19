294cf12589bec4a3e3c211981a0263428cc46872
Ari Holtzman <ahai@cs.washington.edu>
2017-08-19 15:19:00 -0700

    Add some basics to decoders

--- decoders/init.lua
+++ decoders/init.lua
@@ -1,19 +1,18 @@
 require 'Candidate'
+
 local decoders = {}
+
 decoders.beam_search =
     function(model, rnn, dec, width, template)
         assert(#template) == 2
+    
+        -- prepare beam
         local prefix = template[1]
         local suffix = template[2]
-    
-        -- run through prefix
-        local init_hidden = rnn:initializeHidden(1)
-        local inter = model:forward({init_hidden, prefix})
-        local opt_probs, _ = dec:topk(inter, config.k, opts:size(1), config.k)
-        local first_probs, first_idxs = dec:topk(inter, width, 
-        error('Beam Search has not been implemented!') 
-
-        -- prepare beam
+        local inter = model:forward({rnn:initializeHidden(1), prefix})
+        local first_probs, first_idxs = dec:topkseqs(inter, k, prefix)
+        local init_hidden = rnn:getLastHidden()
+        print(init_hidden:size())
         error('Beam Search has not been implemented!') 
         local init_cand = Candidate({ state= }) --finish
         local term = suffix[1]
@@ -28,12 +27,12 @@ decoders.beam_search =
         return best
     end
 
-decoder.template_beam_search =
+decoders.template_beam_search =
     function(model, rnn, width, template)
         error('Template Beam Search has not been implemented!')
     end
 
-decoder.branching_template_beam_search =
+decoders.branching_template_beam_search =
     function(model, rnn, width, template, bf)
         error('Branching Template Beam Search has not been implemented!')
     end
