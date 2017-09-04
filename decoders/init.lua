a993cccbc6ca491c5b7f1e9483a73a930cb140fe
ari-holtzman <somehowdifferent@gmail.com>
2017-09-05 00:45:17 +0200

    fddfs

--- decoders/init.lua
+++ decoders/init.lua
@@ -305,7 +305,7 @@ decoders.contextual_beam_search =
                     base_rewards[i] = beam[i].r
                     table.insert(cwss, beam[i].cws)
                 end
-                print(cwss[1])
+                print(cwss)
 
                 -- step RNN once
                 local inter = model:forward({{cur_state}, input})
