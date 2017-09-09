397955891b40dd77d1297e49df0fb9e5eba3f4e5
ari-holtzman <somehowdifferent@gmail.com>
2017-09-09 14:42:22 +0200

    dfdfs

--- test_roc.lua
+++ test_roc.lua
@@ -108,8 +108,7 @@ while line ~= nil do
     for _, idx in ipairs(ending2) do
       table.insert(option2, idx)
     end
-    print(correctchoice)
-    assert(correcthcoice == 1 or correctchoice == 2)
+    assert((correctcoice == 1) or (correctchoice == 2))
                        
     local choice = decoders.contextual_decider(model2,
                                                rnn,
