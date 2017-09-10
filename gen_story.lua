25c7696210e4a788f2a115436952f8092242be73
ari-holtzman <somehowdifferent@gmail.com>
2017-09-11 00:34:26 +0200

    fdfds

--- gen_story.lua
+++ gen_story.lua
@@ -65,7 +65,7 @@ local model2 = nn.Sequential()
 
 local ne = 0
 io.write('>'); io.flush()
-local line = '<unk> it was a dark and stormy night . </s> '--io.read()
+local line = '<unk> i was alone at home , reading a book . </s> the book was kind of boring . </s> '--io.read()
 local beg = {}
 for word in line:gmatch("[^ ]+") do
     local idx = data.getidx(dic, word)
