795efe8aea7481298a111e13d42324fafec02918
ari-holtzman <somehowdifferent@gmail.com>
2017-09-14 19:53:51 -0700

    sdfdsf

--- convert_dict.py
+++ convert_dict.py
@@ -12,7 +12,7 @@ n_words = 0
 with open(args.dicpath) as dic_file:
     for line in dic_file:
         print(line)
-        index, word, num = line.split()
+        index, word, num = line.split(' ')
         num = int(num)
         if len(freqs) < 2 or num > args.m:
             freqs[word] = num
